b0051552b40ca544408541e32bc69f00f3c46c42
K-y-y-a <90311145+K-y-y-a@users.noreply.github.com>
2021-09-30 18:22:39 +0300

    Update cold-hot.php

--- Task02/cold-hot/bin/cold-hot.php
+++ Task02/cold-hot/bin/cold-hot.php
@@ -8,6 +8,6 @@
         require_once($autoPackagist);
     }
 
-    use function ssiffonn\cold_hot\Controller\startGame;
+    use function K-y-y-a\cold_hot\Controller\startGame;
     startGame();
 ?>
