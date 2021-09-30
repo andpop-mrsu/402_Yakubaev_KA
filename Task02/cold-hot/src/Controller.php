336fc119bbf358ec131de72722d0adb09b7a5d50
K-y-y-a <90311145+K-y-y-a@users.noreply.github.com>
2021-09-30 18:53:04 +0300

    Create Controller.php

---
+++ Task02/cold-hot/src/Controller.php
@@ -0,0 +1,9 @@
+<?php
+    namespace K-y-y-a\cold_hot\Controller;
+    use function K-y-y-a\cold_hot\View\showGame;
+
+    function startGame(){
+        echo "Game started".PHP_EOL;
+        showGame();
+    }
+?>
