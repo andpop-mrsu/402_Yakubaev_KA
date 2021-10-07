b5ce4dbaf6db6b376f1e821f7c7eda53702e2eae
K-y-y-a <90311145+K-y-y-a@users.noreply.github.com>
2021-10-07 17:46:35 +0300

    Update Controller.php

--- Task02/cold-hot/src/Controller.php
+++ Task02/cold-hot/src/Controller.php
@@ -1,6 +1,6 @@
 <?php
-    namespace K-y-y-a\cold_hot\Controller;
-    use function K-y-y-a\cold_hot\View\showGame;
+    namespace kyya\cold_hot\Controller;
+    use function kyya\cold_hot\View\showGame;
 
     function startGame(){
         echo "Game started".PHP_EOL;
