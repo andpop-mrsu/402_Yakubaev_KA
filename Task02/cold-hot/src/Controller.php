6cc0a0cb4360b2d5b9d24a69c232dac9b97d2d22
K-y-y-a <90311145+K-y-y-a@users.noreply.github.com>
2021-11-12 12:48:15 +0300

    Delete Task02/cold-hot directory

--- Task02/cold-hot/src/Controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-    namespace kyya\cold_hot\Controller;
-    use function kyya\cold_hot\View\showGame;
-
-    function startGame(){
-        echo "Game started".PHP_EOL;
-        showGame();
-    }
-?>
