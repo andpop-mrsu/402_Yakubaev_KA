3b40d7c4439b8b7ed47e85336e66eb6970032a73
k-y-y-a <90311145+K-y-y-a@users.noreply.github.com>
2022-02-12 09:14:36 +0300

    Update

--- Task02/Task02_1/tests/VectorTest.php
+++ Task02/Task02_1/tests/VectorTest.php
@@ -12,6 +12,7 @@ class VectorTest extends TestCase
     {
         $v1 = new Vector(1, 2, 3);
         $v2 = new Vector(3, 2, 1);
+
         $this->assertEquals(new Vector(4, 4, 4), $v1->add($v2));
     }
 
@@ -19,6 +20,7 @@ class VectorTest extends TestCase
     {
         $v1 = new Vector(4, 5, 6);
         $v2 = new Vector(7, 8, 9);
+
         $this->assertEquals(
             new Vector(-3, -3, -3), 
             $v1->sub($v2)
@@ -28,6 +30,7 @@ class VectorTest extends TestCase
     public function testProduct()
     {
         $v1 = new Vector(10, 11, 12);
+
         $this->assertEquals(
             new Vector(20, 22, 24), 
             $v1->product(2)
@@ -38,6 +41,7 @@ class VectorTest extends TestCase
     {
         $v1 = new Vector(1, 4, 1);
         $v2 = new Vector(6, 7, 8);
+
         $this->assertEquals(42, $v1->scalarProduct($v2));
     }
 
@@ -45,6 +49,7 @@ class VectorTest extends TestCase
     {
         $v1 = new Vector(13, 14, 15);
         $v2 = new Vector(16, 17, 18);
+
         $this->assertEquals(
             new Vector(-3, 6, -3), 
             $v1->vectorProduct($v2)
