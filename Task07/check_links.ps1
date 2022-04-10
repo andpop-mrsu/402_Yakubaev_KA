d18b7aa8ec3581bd97005c54aceceaa7545b2a40
k-y-y-a <90311145+K-y-y-a@users.noreply.github.com>
2022-04-10 18:22:00 +0300

    Task07

---
+++ Task07/check_links.ps1
@@ -0,0 +1,12 @@
+﻿Get-Item "C:\Users\$env:UserName\Desktop\*.lnk" | ForEach-Object {
+	$BadLinks = "C:\Users\$env:UserName\Desktop\BadLinks\";
+    $WScriptShell = New-Object -COM WScript.Shell;
+    $Shortcut = $WScriptShell.CreateShortcut($_.FullName);
+	
+    If (!(Test-Path $Shortcut.TargetPath)) { 
+        If (!(Test-Path $BadLinks)) { 
+			New-Item -Path $BadLinks -ItemType Directory;
+        } 
+		Move-Item -Path $_.FullName -Destination $BadLinks+$_.Name; 
+    }
+}
---
+++ Task07/excel.ps1
@@ -0,0 +1,10 @@
+﻿$ExcelObject = New-Object -COM Excel.Application;
+$ExcelWorkBook = $ExcelObject.Workbooks.Add();
+$ExcelWorkSheet = $ExcelWorkBook.Worksheets.Item(1);
+$x = 2;
+$y = 2;
+$ExcelWorkSheet.Cells.Item($x,$y) = "Привет от PowerShell";
+$ExcelWorkSheet.Cells.Item($x,$y).Font.size=12;
+$ExcelWorkSheet.Cells.Item($x,$y).Font.Italic = $true;
+$ExcelWorkBook.SaveAs("C:\Users\$env:UserName\Desktop\"+$env:UserName+"_"+$env:ComputerName+".xlsx");
+$ExcelWorkBook.close($true);
